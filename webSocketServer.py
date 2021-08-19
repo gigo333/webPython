50e6e22a3bff32a54c6d5fdb5ba1b2f786762341
gigo333 <gigo.bon@mail.com>
2021-08-19 18:06:40 +0200

    fixed bug in handling 64 bit sizes

--- webSocketServer.py
+++ webSocketServer.py
@@ -1,6 +1,8 @@
 import hashlib
 import base64
 import json
+from threading import Thread
+from struct import pack, unpack
 
 def recvAll(so, l):
     b=b''
@@ -9,32 +11,27 @@ def recvAll(so, l):
 
     return b
 
-def formatWS(toSend, opcode):
+def formatWS(toSend, dType="string"):
+    dType=dType.lower()
+    opcode={"string":1, "text":1, "bin":2, "binary":2, "bytes":2, "buffer":2 }
     length=len(toSend)
     buffer=None
     if(length<126):
         buffer=bytearray(2)
-        buffer[0]=128+opcode
+        buffer[0]=128+opcode[dType]
         buffer[1]=length
-    elif(length<2**16-1): 
+    elif(length<2**16): 
         buffer=bytearray(4)
-        buffer[0]=128+opcode
+        buffer[0]=128+opcode[dType]
         buffer[1]=126
         buffer[2]=length//256
         buffer[3]=length%256
     else:
-        buffer=bytearray(6)
-        opcode=1
-        buffer[0]=128+opcode
+        buffer=bytearray(2)
+        buffer[0]=128+opcode[dType]
         buffer[1]=127
-        buffer[5]=length%256
-        length=length//256
-        buffer[4]=length%256
-        length=length//256
-        buffer[3]=length%256
-        length=length//256
-        buffer[2]=length
-        buffer=bytearray(6)
+        buffer+=pack("!Q", length)
+
     return bytes(buffer)+toSend
 
 def receiveWS(so):
@@ -44,8 +41,8 @@ def receiveWS(so):
         buffer=recvAll(so, 2)
         length=buffer[1]*256+buffer[0]
     elif(length==127):
-        buffer=recvAll(so, 4)
-        length=buffer[3]*(256**3)+buffer[2]*(256**2)+buffer[1]*256+buffer[0]
+        buffer=recvAll(so, 8)
+        length=unpack("!Q", buffer)[0]
 
     buffer=recvAll(so, 4+length)
     mask=buffer[:4]
@@ -61,6 +58,19 @@ Upgrade: websocket\r
 Connection: Upgrade\r
 Sec-WebSocket-Accept: AcceptCode\r
 \r\n"""
+
+def sendThread(so):
+    while(1):
+        try:
+            t=input("\nTemperature?\n")
+            t=t.replace(",",".")
+            obj={"Temperature":float(t), "Humidity":50}
+            toSend=json.dumps(obj).encode()
+            buffer=formatWS(toSend)
+            so.send(buffer)
+        except:
+            break
+
 def handleWS(so, s, code):
     request=s.split(" ")[1][1:].split("?")
     requestPath=request[0]
@@ -68,7 +78,16 @@ def handleWS(so, s, code):
     code=code+"258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
     AcceptCode=base64.b64encode(hashlib.sha1(code.encode()).digest()).decode()
     toSend=wsAcceptHeader.replace("AcceptCode", AcceptCode).encode()
-    so.send(toSend)    
+    so.send(toSend)
+    with open("oldFIles/images/img.jpg", "rb") as f:
+        buffer=f.read()
+        print(len(buffer))
+        b=formatWS(buffer, "Binary")
+        so.sendall(b)
+        
+    th = Thread(target=sendThread, args=(so,))
+    th.setDaemon(True)
+    th.start()    
     while(1):
         try:
             buffer=so.recv(1)
@@ -78,11 +97,7 @@ def handleWS(so, s, code):
                 message=buffer.decode()
                 print("##############")
                 print("WS: "+message)
-                obj={"Temperature":31.5, "Humidity":50}
-                toSend=json.dumps(obj).encode()
-                opcode=1
-                buffer=formatWS(toSend, opcode)
-                so.send(buffer)
+                
         except:
             print("##############")
             print("WS: Disconnected!")
